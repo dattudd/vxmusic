e66aec49444c02845ae4035f9710ffcb96e5fa5f
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-02-02 12:02:00 +0530

    Update start.py

--- AarohiX/plugins/bot/start.py
+++ AarohiX/plugins/bot/start.py
@@ -51,7 +51,7 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = first_page(_)
-            await message.reply_sticker("CAACAgUAAx0CcSyO9gACl7dkQoL0p1vXqcYtCuCHmSyh4Hi1UgACEwgAAonAEVYwaJ0Jkk8L0B4E")
+            await message.reply_sticker("CAACAgUAAxkBAAELExhlkmFbAarTvcZZuxVhtMo0SJcWIwACQwoAAvPiqFR5n0OrwQHpUjQE")
             return await message.reply_photo(
                 photo=config.START_IMG_URL,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
@@ -104,7 +104,7 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        await message.reply_sticker("CAACAgUAAx0CcSyO9gACl7dkQoL0p1vXqcYtCuCHmSyh4Hi1UgACEwgAAonAEVYwaJ0Jkk8L0B4E")
+        await message.reply_sticker("CAACAgUAAxkBAAELExhlkmFbAarTvcZZuxVhtMo0SJcWIwACQwoAAvPiqFR5n0OrwQHpUjQE")
         await message.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
